db49b7fb730838db3bd6a1fa47a8bf50aeaf851e
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-10-20 01:10:16 +0200

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <HelloWorld />
+  <Test />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import Test from './components/Partials/Test.vue';
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Test
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@use "./assets/scss/main.scss";
+
+</style>
